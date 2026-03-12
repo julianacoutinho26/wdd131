a4e74f127552b5d8ccace55d662990688b312cb9
julianacoutinho26 <jrodrigues6@byupathway.edu>
2026-03-12 19:18:02 -0300

    week 1 assigment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+// Ano atual
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Última modificação
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
